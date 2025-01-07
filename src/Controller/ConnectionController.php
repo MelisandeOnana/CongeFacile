0b9af44e78d424ae3f3eee5a8ad850780dd7ed7a
MelisandeOnana <melisande.onana@orange.fr>
2025-01-07 16:56:14 +0100

    Ajout de la fonctionnalité de demande de changement de mot de passe dans le contrôleur de connexion, avec envoi d'un email de notification à l'administrateur.

--- src/Controller/ConnectionController.php
+++ src/Controller/ConnectionController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Exception;
 class ConnectionController extends AbstractController
 {
     #[Route('/', name: 'home_connection', methods: ['GET', 'POST'])]
@@ -30,9 +33,33 @@ class ConnectionController extends AbstractController
     }
 
     #[Route('/MotDePasseOublie', name: 'motdepasseoublie', methods: ['GET'])]
-    public function motdepasseoublie(): Response
+    public function motdepasseoublie(UserRepository $repository, User $user = null): Response
     {
-        return $this->render('default/motdepasseoublie.html.twig');
+        if(isset($_GET['email']) && !empty($_GET['email'])){
+
+            $email = htmlspecialchars($_GET['email']);
+            $user = $repository->findByEmail($email);
+
+            if($user != null){
+
+                $to = "dauguet.mathis@gmail.com";
+                $subject = "CongéFacile :".$user->getPerson()->getFirstName()." ".$user->getPerson()->getLastName()." demande un changement de mot de passe.";
+                $message = "".$user->getPerson()->getFirstName()." ".$user->getPerson()->getLastName()." demande un changement de mot de passe.
+                Adresse email de la personne : “".$email."”.<br>
+                Après changement, merci de notier l’utilisateur de son nouveau mot de passe.";
+                
+                try{
+                    mail($to, $subject, $message);
+                    $reussi = "Demande envoyée";
+                    return $this->render('default/motdepasseoublie.html.twig', ["reussi" => $reussi]);
+                }catch(Exception $e){
+                    $reussi = "Demande non envoyée";
+                    return $this->render('default/motdepasseoublie.html.twig', ["reussi" => $reussi]);
+                }
+            }
+        }
+
+        return $this->render('default/motdepasseoublie.html.twig', ["reussi" => ""]);
     }
 
 }
