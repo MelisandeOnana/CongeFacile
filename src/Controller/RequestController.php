b87f1b7b1f4678e558928d7cfa38241efbafa45b
Allan Decaux <decaux.allan0910@gmail.com>
2025-01-20 17:01:48 +0100

    Filtre - Historique des demandes Collaborateur

--- src/Controller/RequestController.php
+++ src/Controller/RequestController.php
@@ -9,32 +9,32 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use App\Entity\User;
 use App\Repository\RequestRepository;
+use App\Repository\RequestTypeRepository;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Exception;
+
 class RequestController extends AbstractController
 {
     #[Route('/historic', name: 'historic', methods: ['GET'])]
-    public function historic(Request $request, RequestRepository $requestRepository): Response
+    public function historic(Request $request, RequestRepository $requestRepository, RequestTypeRepository $requestTypeRepository): Response
     {
         $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            throw new Exception('L\'utilisateur n\'est pas de type User.');
+        }
         $person = $user->getPerson();
         $requests = $requestRepository->findBy(['collaborator' => $person]);
-        $requestType = $request->query->get('type');
-        $requestDate = $request->query->get('date');
-        $requestStart = $request->query->get('start');
-        $requestEnd = $request->query->get('end');
-        $requestNumber = $request->query->get('number');
-        $requestAnswer = $request->query->get('answer');
+
+        //Filtres
+        $requestType = $requestTypeRepository->findAll();
+
 
         return $this->render('default/historic.html.twig', [
             'requests' => $requests,
-            'type' => $requestType,
-            'date' => $requestDate,
-            'start' => $requestStart,
-            'end' => $requestEnd,
-            'number' => $requestNumber,
-            'answer' => $requestAnswer,
+            'requestTypes' => $requestType,
         ]);
     }
 }
