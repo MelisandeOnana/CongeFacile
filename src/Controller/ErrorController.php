ff28702a1b2df793428d8f8d8ef7948f99de15c9
Allan Decaux <decaux.allan0910@gmail.com>
2025-01-30 11:49:01 +0100

    Pages erreur 403 et 404

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+
+#[AsController]
+class ErrorController extends AbstractController
+{
+    #[Route('/page-not-found', name: 'app_not_found')]
+    public function show404(): Response
+    {
+        return $this->render('exception/404.html.twig', [], new Response('', Response::HTTP_NOT_FOUND));
+    }
+
+    #[Route('/access-denied', name: 'app_access_denied')]
+    public function show403(): Response
+    {
+        return $this->render('exception/403.html.twig', [], new Response('', Response::HTTP_FORBIDDEN));
+    }
+}
