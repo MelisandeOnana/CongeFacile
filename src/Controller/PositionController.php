9441c795fcddfffabeaede5e9f8d5cf5abd30e46
Allan Decaux <decaux.allan0910@gmail.com>
2025-02-18 14:16:16 +0100

    Ajouter/Modifier/Supprimer un poste

--- src/Controller/PositionController.php
+++ src/Controller/PositionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Position;
 use App\Repository\PersonRepository;
 use App\Repository\PositionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,22 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request as HttpRequest;
 use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\PositionType;
+use App\Form\DeleteType;
 
 class PositionController extends AbstractController
 {
     #[Route('/admin/positions', name: 'positions')]
     public function index(PositionRepository $positionRepository, PersonRepository $personRepository,  PaginatorInterface $paginator, HttpRequest $request): Response
     {
-        $positions  = $positionRepository->findAll();
         $positionCounts = [];
 
         // Récupérer les valeurs des filtres depuis la requête
         $filterName = $request->query->get('name');
         $filterNumber = $request->query->get('number');
-
-        foreach ($positions as $position) {
-            $positionCounts[$position->getId()] = $personRepository->countByPosition($position);
-        }
 
         $criteria = Criteria::create();
 
@@ -33,7 +32,12 @@ class PositionController extends AbstractController
             $criteria->andWhere(Criteria::expr()->contains('name', $filterName));
         }
 
+        $criteria->orderBy(['id' => 'DESC']);
         $filteredPositions = $positionRepository->matching($criteria);
+
+        foreach ($filteredPositions as $position) {
+            $positionCounts[$position->getId()] = $personRepository->countByPosition($position);
+        }
 
         if ($filterNumber != null) {
             $filteredPositionsByNumber = [];
@@ -54,6 +58,78 @@ class PositionController extends AbstractController
         return $this->render('default/administration/position/positions.html.twig', [
             'positions' => $PostionsPagination,
             'positionCounts' => $positionCounts,
+        ]);
+    }
+
+    #[Route('/admin/position/show/{id}', name: 'position_show')]
+    public function show(PositionRepository $positionRepository, $id, HttpRequest $request, EntityManagerInterface $entityManager, PersonRepository $personRepository): Response
+    {
+        $position = $positionRepository->find($id);
+
+        if (!$position) {
+            throw $this->createNotFoundException('Le poste n\'existe pas.');
+        }
+
+        $positionCount = $personRepository->countByPosition($position);
+
+        $delete = $request->query->get('delete');
+        $formPosition = $this->createForm(PositionType::class, $position);
+        $formPosition->handleRequest($request);
+        $formDelete = $this->createForm(DeleteType::class);
+        $formDelete->handleRequest($request);
+
+        if ($delete == 'true') {
+            if ($formDelete->isSubmitted() && $formDelete->isValid()) {
+                if ($positionCount > 0) {
+                    $this->addFlash('error', 'Impossible de supprimer ce poste car il est associé à des collaborateurs.');
+                    return $this->redirectToRoute('position_show', ['id' => $id]);
+                } else {
+                    $entityManager->remove($position);
+                    $entityManager->flush();
+                    return $this->redirectToRoute('positions');
+                }
+            }
+        }
+
+        if ($formPosition->isSubmitted() && $formPosition->isValid()) {
+            $existingPosition = $positionRepository->findOneBy(['name' => $position->getName()]);
+            if ($existingPosition && $existingPosition->getId() !== $position->getId()) {
+                $this->addFlash('error', 'Un poste avec ce nom existe déjà.');
+                return $this->redirectToRoute('position_show', ['id' => $id]);
+            } else {
+
+                $entityManager->persist($position);
+                $entityManager->flush();
+                return $this->redirectToRoute('positions');
+            }
+        }
+
+        return $this->render('default/administration/position/position_show.html.twig', [
+            'position' => $position,
+            'formPosition' => $formPosition->createView(),
+            'formDelete' => $formDelete->createView(),
+        ]);
+    }
+
+    #[Route('/admin/position/new', name: 'position_new')]
+    public function new(PositionRepository $positionRepository, HttpRequest $request, EntityManagerInterface $entityManager): Response
+    {
+        $position = new Position();
+        $formPosition = $this->createForm(PositionType::class, $position);
+        $formPosition->handleRequest($request);
+
+        if ($formPosition->isSubmitted() && $formPosition->isValid()) {
+            if ($positionRepository->findOneBy(['name' => $position->getName()])) {
+                $this->addFlash('error', 'Un poste avec ce nom existe déjà.');
+            } else {
+                $entityManager->persist($position);
+                $entityManager->flush();
+                return $this->redirectToRoute('positions');
+            }
+        }
+
+        return $this->render('default/administration/position/position_new.html.twig', [
+            'formPosition' => $formPosition->createView(),
         ]);
     }
 }
---
+++ src/Form/PositionType.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Position;
+
+class PositionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('name', TextType::class, [
+            'required' => true,
+            'label' => 'Nom du poste',
+            'label_attr' => ['class' => 'block text-[16px] font-medium font-[Inter]'],
+            'attr' => [
+                'class' => 'w-[350px] h-[46px] border rounded-[6px] pl-6 pr-6 mt-4',
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Position::class,
+        ]);
+    }
+}
