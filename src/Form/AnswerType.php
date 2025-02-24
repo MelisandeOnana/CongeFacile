19aa1d1ac05d4e5963dfd1618293dbadb954ea68
Allan Decaux <decaux.allan0910@gmail.com>
2025-02-24 15:10:14 +0100

    Réparation page historique collaborateur et suppression des use inutile

--- src/Form/AnswerType.php
+++ src/Form/AnswerType.php
@@ -3,19 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Request;
-use App\Entity\RequestType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use DateTime;
-use Symfony\Component\Form\FormError;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 
 class AnswerType extends AbstractType
 {
--- src/Form/PreferencesType.php
+++ src/Form/PreferencesType.php
@@ -2,10 +2,7 @@
 
 namespace App\Form;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
