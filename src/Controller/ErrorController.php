409faa833acfe44201d852c6bc3a5c9e46e85d69
MelisandeOnana <melisande.onana@orange.fr>
2025-03-17 10:16:07 +0100

    Ajout d'un contrôleur d'erreurs pour gérer les erreurs 404 et 403, mise à jour des routes et création d'un modèle de page d'erreur générique.

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -3,22 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpFoundation\Request;
+use Throwable;
 
-#[AsController]
 class ErrorController extends AbstractController
 {
-    #[Route('/page-not-found', name: 'app_not_found')]
-    public function show404(): Response
+    public function show(Request $request, Throwable $exception): Response
     {
-        return $this->render('exception/404.html.twig', [], new Response('', Response::HTTP_NOT_FOUND));
-    }
+        $statusCode = $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
 
-    #[Route('/access-denied', name: 'app_access_denied')]
-    public function show403(): Response
-    {
-        return $this->render('exception/403.html.twig', [], new Response('', Response::HTTP_FORBIDDEN));
+        if ($statusCode == Response::HTTP_NOT_FOUND) {
+            return $this->render('exception/404.html.twig', [], new Response('', Response::HTTP_NOT_FOUND));
+        }
+
+        if ($statusCode == Response::HTTP_FORBIDDEN) {
+            return $this->render('exception/403.html.twig', [], new Response('', Response::HTTP_FORBIDDEN));
+        }
+
+        return $this->render('exception/error.html.twig', ['status_code' => $statusCode]);
     }
 }
