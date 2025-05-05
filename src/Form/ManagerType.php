486dd5bdf83b38517e89e3b0cb5a2fa883ec9889
LeMathoux <123992331+LeMathoux@users.noreply.github.com>
2025-05-05 09:58:38 +0200

    fix manager update

--- src/Form/ManagerType.php
+++ src/Form/ManagerType.php
@@ -53,30 +53,34 @@ class ManagerType extends AbstractType
             ->add('newPassword', PasswordType::class, [
                 'label' => 'Nouveau mot de passe',
                 'label_attr' => ['class' => 'mb-[10px] block text-sm font-medium text-[#111928]'],
-                'required' => true,
+                'required' => $options['require_password'],
                 'mapped' => false,
                 'attr' => [
                     'class' => 'mb-[15px] block w-[350px] h-[46px] px-3 py-2 rounded-[6px] border-[1px] border-[#E5E7EB]',
                 ],
-                'constraints' => [
-                    new Assert\NotBlank(['message' => 'Veuillez saisir un mot de passe.']),
+                'constraints' => $options['require_password'] ? [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir un mot de passe.',
+                    ]),
                     new Assert\Length([
                         'min' => 8,
-                        'minMessage' => 'Le mot de passe doit contenir au moins 8 caractères.',
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères.',
                     ]),
-                ],
+                ] : [],
             ])
             ->add('confirmPassword', PasswordType::class, [
                 'label' => 'Confirmation de mot de passe',
                 'label_attr' => ['class' => 'mb-[10px] block text-sm font-medium text-[#111928]'],
-                'required' => true,
+                'required' => $options['require_password'],
                 'mapped' => false,
                 'attr' => [
                     'class' => 'mb-[15px] block w-[350px] h-[46px] px-3 py-2 rounded-[6px] border-[1px] border-[#E5E7EB]',
                 ],
-                'constraints' => [
-                    new Assert\NotBlank(['message' => 'Veuillez confirmer le mot de passe.']),
-                ],
+                'constraints' => $options['require_password'] ? [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez confirmer le mot de passe.',
+                    ]),
+                ] : [],
             ]);
 
         $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
@@ -100,6 +104,7 @@ class ManagerType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'require_password' => true,
         ]);
     }
 }
