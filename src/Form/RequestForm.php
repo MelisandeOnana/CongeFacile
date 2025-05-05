27a65233389b89c24134e8843efb36abb4a32a76
Allan Decaux <decaux.allan0910@gmail.com>
2025-05-05 12:14:41 +0200

    Fix commit

--- src/Form/RequestForm.php
+++ src/Form/RequestForm.php
@@ -10,8 +10,6 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Constraints\File;
-use Symfony\Component\Validator\Constraints\Callback;
-use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class RequestForm extends AbstractType
 {
@@ -59,7 +57,7 @@ class RequestForm extends AbstractType
             'constraints' => [
                 new Assert\NotNull(['message' => 'La date de fin est obligatoire.']),
                 new Assert\Expression([
-                    'expression' => 'this.getStartAt() < this.getEndAt()',
+                    'expression' => 'value > this.getParent().get("startAt").getData()',
                     'message' => 'La date de fin doit être postérieure à la date de début.',
                 ]),
             ],
@@ -102,18 +100,5 @@ class RequestForm extends AbstractType
                 ]),
             ],
         ]);
-    }
-
-    // Méthode de validation personnalisée
-    public function validateDates($value, ExecutionContextInterface $context)
-    {
-        $form = $context->getRoot(); // Récupère le formulaire parent
-        $startAt = $form->get('startAt')->getData();
-        $endAt = $value;
-
-        if ($startAt && $endAt && $startAt >= $endAt) {
-            $context->buildViolation('La date de fin doit être postérieure à la date de début.')
-                ->addViolation();
-        }
     }
 }
